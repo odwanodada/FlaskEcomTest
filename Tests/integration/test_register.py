ec1b79d1a7bae0716074eb2098651b8e3ab18359
odwanodada <odwanodada22@gmail.com>
2021-06-14 16:28:47 +0200

    Testing

--- Tests/integration/test_register.py
+++ Tests/integration/test_register.py
@@ -16,5 +16,24 @@ class TestRegister(BaseTest):
 
 
                 self.assertTrue(user)
-                self.assertIn(b'Account created successfully! You are now logged in as', response.data)
+                self.assertTrue(User)
+                self.assertIn(b'Account created successfully! You are now logged in as JoeDoe', response.data)
                 self.assertEqual('http://localhost/market', request.url)
+
+
+    def test_password_dont_match(self):
+        with self.app:
+            with self.app_context:
+                response = self.app.post('/register',
+                                         data=dict(username="Odwa", email_address="odwa@gmail.com",
+                                                   password1="1234567", password2="1234567", ), follow_redirects=True)
+
+
+                user = db.session.query(User).filter_by(email_address="odwa@gmail.com").first()
+
+
+                self.assertFalse(user)
+
+
+                self.assertIn(b'There was an error with creating a user: '
+                              b'[&#39;Field must be equal to password1.&#39;]', response.data)
--- market/routes.py
+++ market/routes.py
@@ -5,6 +5,7 @@ from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required, current_user
 
+
 @app.route('/')
 @app.route('/home')
 def home_page():
